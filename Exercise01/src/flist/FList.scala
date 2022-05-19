f648b882891c983dba906ff3cb4fda63d0b1c0e3
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2022-05-19 19:05:44 +0200

    renames list to tail

--- Exercise01/src/flist/FList.scala
+++ Exercise01/src/flist/FList.scala
@@ -6,7 +6,7 @@ trait FList[+A] {
 
   def map[B](mapFunction: A => B): FList[B] =
     this match
-      case FCons(head, list) => FCons[B](mapFunction(head), list.map(mapFunction))
+      case FCons(head, tail) => FCons[B](mapFunction(head), tail.map(mapFunction))
       case _ => FNil[B]()
 }
 
@@ -14,6 +14,6 @@ case class FNil[A]() extends FList[A]:
   override def size: Int = 0
   override def isEmpty: Boolean = true
 
-case class FCons[A](head: A, list: FList[A]) extends FList[A]:
-  override def size: Int = 1 + list.size
+case class FCons[A](head: A, tail: FList[A]) extends FList[A]:
+  override def size: Int = 1 + tail.size
   override def isEmpty: Boolean = false
