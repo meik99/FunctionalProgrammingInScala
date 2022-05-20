b89d4a035f328b66871248bc00d65d796acad11d
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2022-05-20 11:28:19 +0200

    adds BinTree case classes

--- Exercise02/src/tree/BinTree.scala
+++ Exercise02/src/tree/BinTree.scala
@@ -1,5 +1,14 @@
 package tree
 
-trait BinTree {
-
+trait BinTree[+A] {
+  def isEmpty: Boolean
 }
+
+case class EmptyTree[+A]() extends BinTree[A]:
+  override def isEmpty: Boolean = true
+
+case class BinNode[+A](elem: A, left: BinTree[A], right: BinTree[A]) extends BinTree[A]:
+  override def isEmpty: Boolean = elem match
+    case BinNode(_, _, _) => false
+    case _ => true
+
---
+++ Exercise02/test/tree/BinTreeSuite.scala
@@ -0,0 +1,5 @@
+package tree
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class BinTreeSuite extends AnyFunSuite:
