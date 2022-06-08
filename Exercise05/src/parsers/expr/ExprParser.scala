905b1d5c102fd9c500173e34f444608f4e4d8331
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2022-06-08 17:59:12 +0200

    adds exercise 05

--- Exercise05/src/parsers/expr/ExprParser.scala
+++ Exercise05/src/parsers/expr/ExprParser.scala
@@ -1,6 +1,6 @@
 package parsers.expr
 
-import expr.Expr
+import expr.{Expr, Lit, Mult, Var}
 
 import scala.util.parsing.combinator.JavaTokenParsers
 import scala.util.parsing.input.{CharSequenceReader, PagedSeqReader}
@@ -10,7 +10,6 @@ object ExprParser extends JavaTokenParsers {
 
   // I have no idea where to even begin
   def expr : Parser[Expr] = ???
-
 }
 
 object ExprParserApp {
